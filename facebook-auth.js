d85ff6e6c429c0638c6e1aab5c01a36685b7f546
Luke Madera <luke.madera@gmail.com>
2013-11-03 09:36:13 -0800

    'init'

---
+++ facebook-auth.js
@@ -0,0 +1,17 @@
+/**
+@fileOverview
+
+@toc
+
+*/
+
+'use strict';
+
+angular.module('jackrabbitsgroup.angular-facebook-auth', [])
+.factory('jrgFacebookAuth', [ function () {
+	//private methods and properties - should ONLY expose methods and properties publicly (via the 'return' object) that are supposed to be used; everything else (helper methods that aren't supposed to be called externally) should be private.
+	
+	//public methods & properties
+	return {
+	};
+}]);
---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'jrgFacebookAuth', function($scope, jrgFacebookAuth) {
+	//TODO - put any directive code here
+}]);
